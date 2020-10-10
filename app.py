01579c60890302544f58eaf58f070260ac660943
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2020-10-10 14:31:34 +0530

    completed gen csv and other

--- app.py
+++ app.py
@@ -1,123 +1,191 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, render_template, request, url_for, redirect, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-from config import DevelopmentConfig
 from sshtunnel import SSHTunnelForwarder
 from woocommerce import API
 from sqlalchemy.dialects.postgresql import UUID
-from custom import filter_orders, list_order_items, list_order_refunds, list_only_refunds
+from custom import filter_orders, list_order_items, list_order_refunds, list_only_refunds, get_totals, get_params, get_orders_with_messages, list_order_items_csv
 from flask_datepicker import datepicker
 from werkzeug.datastructures import ImmutableMultiDict
+from datetime import datetime
 import uuid
-import datetime
-import pprint
+import json
+import requests
+import csv
+import os
 
 
 app = Flask(__name__, instance_relative_config=True)
 datepicker(app)
 
-app.config.from_object(DevelopmentConfig)
-db = SQLAlchemy(app)
+app.config.from_pyfile("config.py")
+
+# db = SQLAlchemy(app)
 wcapi = API(
-    url="http://box2364.temp.domains/~leapclub/",
-    consumer_key="ck_785fefd21aa061e99511b6fe01a69671aa289100",
-    consumer_secret="cs_35e700a4c2f1b325470f5f64d676608a3e892c6d",
+    url=app.config["WOOCOMMERCE_API_URL"],
+    consumer_key=app.config["WOOCOMMERCE_API_CUSTOMER_KEY"],
+    consumer_secret=app.config["WOOCOMMERCE_API_CUSTOMER_SECRET"],
     version="wc/v3"
 )
 
 
-class Orders(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow())
-    date_modified = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow())
-    total = db.Column(db.Float)
-    firstname = db.Column(db.String)
-    lastname = db.Column(db.String)
-    city = db.Column(db.String)
-    phone_number = db.Column(db.String)
-    address = db.Column(db.String)
-    address2 = db.Column(db.String)
-    payment_method = db.Column(db.String)
-    date_paid = db.Column(db.String)
-    whatsapp_message = db.Column(db.Text)
+# class Orders(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date_created = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow())
+#     date_modified = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow())
+#     total = db.Column(db.Float)
+#     firstname = db.Column(db.String)
+#     lastname = db.Column(db.String)
+#     city = db.Column(db.String)
+#     phone_number = db.Column(db.String)
+#     address = db.Column(db.String)
+#     address2 = db.Column(db.String)
+#     payment_method = db.Column(db.String)
+#     date_paid = db.Column(db.String)
+#     whatsapp_message = db.Column(db.Text)
 
 
+# @app.route("/", methods=["GET", "POST"])
+# def webhooks():
+#     if request.method == "GET":
+#         page = request.args.get("page", 1, type=int)
+#         orders = Orders.query.paginate(page=page, per_page=50)
+#         return render_template("orders.html", orders=orders)
+#     else:
+#         data = request.get_json()
+#         if data != []:
+#             c_msg = "Hi - Your order is on the way.\n\nPlease check whether you are satisfied with the quality of items when you receive them. Feel free to Whatsapp me here if you have any queries.\n\nI hope you will like them. Looking forward to your feedback. :)\n\n\nHere are the order details:\n\n"
+#             c_msg = c_msg + list_order_items_without_refunds(
+#                 data["line_items"], o["id"]) + "*Total Amount: "+data["total"]+"*"
+#             order = Orders(
+#                 id=data["id"],
+#                 date_created=data["date_created"],
+#                 date_modified=data["date_modified"],
+#                 total=data["total"],
+#                 firstname=data["billing"]["first_name"],
+#                 lastname=data["billing"]["last_name"],
+#                 city=data["billing"]["city"],
+#                 phone_number=data["billing"]["phone"],
+#                 address=data["billing"]["address_1"],
+#                 address2=data["billing"]["address_2"],
+#                 payment_method=data["payment_method"],
+#                 date_paid=data["date_paid"],
+#                 whatsapp_message=c_msg
+#             )
+#             db.session.add(order)
+#             db.session.commit()
+#             return {"status": "Success..."}
+#         else:
+#             return {"data": "error"}
 @app.route("/", methods=["GET", "POST"])
 def webhooks():
-    if request.method == "GET":
-        page = request.args.get("page", 1, type=int)
-        orders = Orders.query.paginate(page=page, per_page=50)
-        return render_template("orders.html", orders=orders)
-    else:
-        data = request.get_json()
-        if data != []:
-            c_msg = "Hi - Your order is on the way.\n\nPlease check whether you are satisfied with the quality of items when you receive them. Feel free to Whatsapp me here if you have any queries.\n\nI hope you will like them. Looking forward to your feedback. :)\n\n\nHere are the order details:\n\n"
-            c_msg = c_msg + list_order_items_without_refunds(
-                data["line_items"], o["id"]) + "*Total Amount: "+data["total"]+"*"
-            order = Orders(
-                id=data["id"],
-                date_created=data["date_created"],
-                date_modified=data["date_modified"],
-                total=data["total"],
-                firstname=data["billing"]["first_name"],
-                lastname=data["billing"]["last_name"],
-                city=data["billing"]["city"],
-                phone_number=data["billing"]["phone"],
-                address=data["billing"]["address_1"],
-                address2=data["billing"]["address_2"],
-                payment_method=data["payment_method"],
-                date_paid=data["date_paid"],
-                whatsapp_message=c_msg
-            )
-            db.session.add(order)
-            db.session.commit()
-            return {"status": "Success..."}
-        else:
-            return {"data": "error"}
+    return redirect(url_for("woocom_orders"))
 
 
 @app.route("/orders")
 def woocom_orders():
     args = request.args.to_dict(flat=False)
     new_ids = []
-    if "order_id" in args:
-        for i in args["order_id"]:
+    if "order_ids" in args:
+        for i in args["order_ids"]:
             new_ids.append(int(i))
-    args["order_id"] = new_ids
-    orders = wcapi.get("orders").json()
-    f_orders = filter_orders(orders, args)
-    for o in f_orders:
-        order_refunds = []
-        if len(o["refunds"]) > 0:
-            order_refunds = wcapi.get("orders/"+str(o["id"])+"/refunds").json()
-        c_msg = "Here are the order details:\n\n" + \
-            list_order_items(o["line_items"], order_refunds) + \
-            "*Total Amount: "+o["total"]+"*\n\n"
-        if len(o["refunds"]) > 0:
-            c_msg = c_msg+ \
-                list_order_refunds(order_refunds)+list_only_refunds(order_refunds)
-        s_msg = ("Order ID: "+str(o["id"])
-                 + "\n\nName: "+o["billing"]["first_name"] +
-                 " "+o["billing"]["last_name"]
-                 + "\nMobile: "+o["billing"]["phone"]
-                 + "\n\nAddress: "+o["billing"]["address_1"] +
-                 ", "+o["billing"]["address_2"]
-                 + "\n\nTotal Amount: "+str(o["total"])
-                 + "\n\n"+list_order_items(o["line_items"], order_refunds)
-                 + "Payment Status: Paid To LeapClub.")
-        o["c_msg"] = c_msg
-        o["s_msg"] = s_msg
-    if type(orders) == list:
-        for o in orders:
-            refunds = 0
-            for r in o["refunds"]:
-                refunds = refunds + float(r["total"])
-            o["total_refunds"] = refunds*-1
-            o["total"] = float(o["total"])
-        return render_template("woocom_orders.html", orders=f_orders, query=args)
+    args["order_ids"] = new_ids
+    if "w_status" in args:
+        is_w = True
+        w_status = args["w_status"]
     else:
-        return orders
+        is_w = False
+        w_status = ""
+    params = get_params(args)
+    orders = wcapi.get("orders", params=params).json()
+    f_orders = filter_orders(orders, args)
+    orders = get_orders_with_messages(f_orders, wcapi)
+    for o in orders:
+        refunds = 0
+        for r in o["refunds"]:
+            refunds = refunds + float(r["total"])
+        o["total_refunds"] = refunds*-1
+        o["total"] = float(o["total"])
+    return render_template("woocom_orders.html", orders=orders, query=args, nav_active=params["status"], is_w=is_w, w_status=w_status)
+
+
+def send_whatsapp_msg(order):
+    url = "https://live-server-54.wati.io/api/v1/sendTemplateMessage/" + \
+        order["billing"]["phone"]
+
+    payload = {
+        "template_name": "order_intro_2308",
+        "broadcast_name": "order_ack",
+        "parameters": "[{'name':'name', 'value':'"+order["billing"]["first_name"]+" " + order["billing"]["last_name"]+"'},{'name':'manager', 'value':'Pratik'}]"
+    }
+    headers = {
+        'Authorization': app.config["WATI_AUTHORIZATION"],
+        'Content-Type': 'application/json',
+        'Cookie': '__cfduid=d4f3524497b15c8e8434c2cb0cf4d47601602064478'
+    }
+
+    response = requests.request(
+        "POST", url, headers=headers, data=json.dumps(payload))
+
+    return json.loads(response.text.encode('utf8'))
+
+
+@app.route("/send_whatsapp_msg/<string:order_id>")
+def send_whatsapp(order_id):
+    args = request.args.to_dict(flat=False)
+    if "status" in args:
+        nav_active = args["status"][0]
+    else:
+        nav_active = "any"
+    order = wcapi.get("orders/"+order_id).json()
+    result = send_whatsapp_msg(order)
+    if result["result"] == "success":
+        w_status = "Message Sent."
+    else:
+        w_status = "Message Not Sent."
+    if nav_active != "any":
+        return redirect(url_for("woocom_orders", w_status=w_status, status=nav_active))
+    else:
+        return redirect(url_for("woocom_orders", w_status=w_status))
+
+
+def get_csv_from_orders(orders):
+    f = open("sample.csv", "w+")
+    writer = csv.DictWriter(
+        f, fieldnames=["Order ID", "Customer Detail", "Total Amount", "Order Details", "Comments"])
+    writer.writeheader()
+    for o in orders:
+        refunds = []
+        if len(o["refunds"])>0:
+            refunds = wcapi.get("orders/"+str(o["id"])+"/refunds").json()
+        writer.writerow({
+            "Order ID": o["id"],
+            "Customer Detail": "Name: "+o["billing"]["first_name"]+" "+o["billing"]["last_name"]+"\nMobile: "+o["billing"]["phone"]+"\nAddress: "+o["billing"]["address_1"]+", "+o["billing"]["address_2"],
+            "Total Amount": get_totals(o["total"], refunds),
+            "Order Details": list_order_items_csv(o["line_items"], refunds),
+            "Comments": "Payment Status: Paid To Leap"
+        })
+        writer.writerow({})
+    f.close()
+    f = open("sample.csv", "r")
+    result = f.read()
+    os.remove("sample.csv")
+    return result
+
+
+@app.route('/csv', methods=["POST"])
+def download_csv():
+    data = request.form.to_dict(flat=False)
+    orders = wcapi.get("orders", params=get_params(data)).json()
+    csv_text = get_csv_from_orders(orders)
+    filename = str(datetime.utcnow())+"-" + data["status"][0]+".csv"
+    response = make_response(csv_text)
+    cd = 'attachment; filename='+filename
+    response.headers['Content-Disposition'] = cd
+    response.mimetype = 'text/csv'
+
+    return response
 
 
 if __name__ == "__main__":
-    db.create_all()
+    # db.create_all()
     app.run()
--- custom.py
+++ custom.py
@@ -1,16 +1,10 @@
 def filter_orders(orders, params):
-    if len(params) <= 1:
+    if len(params) <= 3:
         return orders
     f_orders = []
     for o in orders:
-        c = {"status": False, "payment_status": False,
-             "phone_number": False, "name": False, "order_id": False}
-        if "status" in params:
-            if params["status"][0] != "":
-                if params["status"][0] == o["status"]:
-                    c["status"] = True
-            else:
-                c["status"] = True
+        c = {"payment_status": False,
+             "phone_number": False, "name": False}
 
         if "payment_status" in params:
             if params["payment_status"][0] != "":
@@ -34,13 +28,7 @@ def filter_orders(orders, params):
                     c["name"] = True
             else:
                 c["name"] = True
-        if "order_id" in params:
-            if params["order_id"] != []:
-                if o["id"] in params["order_id"]:
-                    c["order_id"] = True
-            else:
-                c["order_id"] = True
-        if c["status"] and c["payment_status"] and c["phone_number"] and c["name"] and c["order_id"]:
+        if c["payment_status"] and c["phone_number"] and c["name"]:
             f_orders.append(o)
     return f_orders
 
@@ -93,7 +81,7 @@ def list_order_refunds(order_refunds):
             + str(order_item["subtotal"])[1:]
             + "\n\n"
         )
-    if len(msg)>0:
+    if len(msg) > 0:
         msg = "We are not able to send: \n\n"+msg
     return msg
 
@@ -107,9 +95,11 @@ def list_order_items(order_items, refunds):
     for order_item in order_items:
         for refund in total_refunds:
             if order_item["id"] == int(refund["meta_data"][0]["value"]):
-                order_item["quantity"] = order_item["quantity"] + refund["quantity"]
-                order_item["total"] = float(order_item["total"]) + float(refund["total"])
-        if order_item["quantity"]>0:
+                order_item["quantity"] = order_item["quantity"] + \
+                    refund["quantity"]
+                order_item["total"] = float(
+                    order_item["total"]) + float(refund["total"])
+        if order_item["quantity"] > 0:
             msg = (
                 msg
                 + order_item["name"]
@@ -126,9 +116,87 @@ def list_order_items(order_items, refunds):
             )
     return msg
 
+def list_order_items_csv(order_items, refunds):
+    msg = ""
+    total_refunds = []
+    for r in refunds:
+        for ri in r["line_items"]:
+            total_refunds.append(ri)
+    for order_item in order_items:
+        for refund in total_refunds:
+            if order_item["id"] == int(refund["meta_data"][0]["value"]):
+                order_item["quantity"] = order_item["quantity"] + \
+                    refund["quantity"]
+                order_item["total"] = float(
+                    order_item["total"]) + float(refund["total"])
+        if order_item["quantity"] > 0:
+            msg = (
+                msg
+                + order_item["name"]
+                + " x "
+                + str(order_item["quantity"])
+                + " = "
+                + str(order_item["total"])
+                + "\n\n"
+            )
+    return msg
+
+
 def list_only_refunds(order_refunds):
     msg = ""
     for r in order_refunds:
-        if len(r["line_items"])==0:
+        if len(r["line_items"]) == 0:
             msg = msg+r["reason"]+": "+r["amount"]+"\n\n"
     return msg
+
+
+def get_totals(total, refunds):
+    msg = total
+    for r in refunds:
+        msg = msg+" - " + str(r["amount"])
+        total = float(total) - float(r["amount"])
+    if len(refunds) > 0:
+        msg = msg+" = "+str(total)
+    return str(total)
+
+
+def get_params(args):
+    params = {}
+    if "status" in args:
+        params["status"] = args["status"][0]
+    else:
+        params["status"] = "any"
+
+    if "order_ids" in args:
+        id_text = ""
+        for id in args["order_ids"]:
+            id_text = id_text+str(id)+", "
+        params["include"] = id_text[:-2]
+    return params
+
+
+def get_orders_with_messages(orders, wcapi):
+    orders = orders
+    for o in orders:
+        order_refunds = []
+        if len(o["refunds"]) > 0:
+            order_refunds = wcapi.get("orders/"+str(o["id"])+"/refunds").json()
+        c_msg = "Here are the order details:\n\n" + \
+            list_order_items(o["line_items"], order_refunds) + \
+            "*Total Amount: "+get_totals(o["total"], order_refunds)+"*\n\n"
+        if len(o["refunds"]) > 0:
+            c_msg = c_msg + \
+                list_order_refunds(order_refunds) + \
+                list_only_refunds(order_refunds)
+        s_msg = ("Order ID: "+str(o["id"])
+                 + "\n\nName: "+o["billing"]["first_name"] +
+                 " "+o["billing"]["last_name"]
+                 + "\nMobile: "+o["billing"]["phone"]
+                 + "\n\nAddress: "+o["billing"]["address_1"] +
+                 ", "+o["billing"]["address_2"]
+                 + "\n\nTotal Amount: "+get_totals(o["total"], order_refunds)
+                 + "\n\n"+list_order_items(o["line_items"], order_refunds)
+                 + "Payment Status: Paid To LeapClub.")
+        o["c_msg"] = c_msg
+        o["s_msg"] = s_msg
+    return orders
