39c9b99360bef91c1cd46e23aecc98fdc982f64e
shubhbansal <shubhamindia18@gmail.com>
2020-11-08 15:04:24 +0530

    modified template

--- template_broadcast.py
+++ template_broadcast.py
@@ -2,11 +2,15 @@ TemplatesBroadcast = {
     "hello_msg": {
         'bakery': {"template": "order_intro_2910", "broadcast": "order_ack"},
         'grocery': {"template": "order_intro_2910", "broadcast": "order_ack"},
-        'personal_care': {"template": "order_intro_2910", "broadcast": "order_ack"}
+        'personal_care': {"template": "order_intro_2910", "broadcast": "order_ack"},
+        'dairy': {"template": "order_intro_2910", "broadcast": "order_ack"}
+
     },
     'order_detail_msg': {
         'bakery':  {"template": "order_details_0211_2", "broadcast": "order_summary"},
         'grocery': {"template": "order_details_0211_2", "broadcast": "order_summary"},
+        'dairy': {"template": "order_details_0211_2", "broadcast": "order_summary"},
+
         # 'personal_care': {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
 
     },
@@ -14,32 +18,44 @@ TemplatesBroadcast = {
         'bakery':    {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
         'grocery': {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
         'personal_care': {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
+                'dairy': {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
+
     },
     "delivery_today_msg": {
         'bakery':    {"template": "delivery_today", "broadcast": "delivery_notification"},
         'grocery': {"template": "delivery_today", "broadcast": "delivery_notification"},
         'personal_care': {"template": "delivery_today", "broadcast": "delivery_notification"},
+                'dairy': {"template": "delivery_today", "broadcast": "delivery_notification"},
+
     },
     "payment_remainder_msg": {
         'bakery': {"template": "payment_reminder", "broadcast": "payment_reminder"},
         'grocery': {"template": "payment_reminder", "broadcast": "payment_reminder"},
         'personal_care': {"template": "payment_reminder", "broadcast": "payment_reminder"},
+                'dairy': {"template": "payment_reminder", "broadcast": "payment_reminder"},
+
 
     },
     "feedback_msg": {
         'bakery': {"template": "feed_past", "broadcast": "feedback"},
         'grocery': {"template": "feed_past", "broadcast": "feedback"},
-        'personal_care': {"template": "feed_past", "broadcast": "feedback"}
+        'personal_care': {"template": "feed_past", "broadcast": "feedback"},
+                'dairy': {"template": "feed_past", "broadcast": "feedback"}
+
     },
     'prepay_msg': {
         'bakery':  {"template": "reply_order", "broadcast": "prepay"},
         'grocery': {"template": "reply_order", "broadcast": "prepay"},
         # 'personal_care': {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
+                'dairy': {"template": "reply_order", "broadcast": "prepay"},
+
 
     },
     'postpay_msg': {
         'bakery':  {"template": "postpay_0211_1", "broadcast": "postpay"},
         'grocery': {"template": "postpay_0211_1", "broadcast": "postpay"},
+                'dairy': {"template": "postpay_0211_1", "broadcast": "postpay"},
+
         # 'personal_care': {"template": "delivery_tomorrow", "broadcast": "delivery_notification"},
 
     },
@@ -52,5 +68,7 @@ vendor_type = {
     'The Womens Company': 'personal_care',
     'desiutpadbyjaya': "grocery",
     'organicgermanbakeshop': 'bakery',
-    'micheartisanbakery': 'bakery'
+    'micheartisanbakery': 'bakery',
+    'mrdairy':'dairy',
+    'Mr. Dairy': 'dairy',
 }
