e304d0d6f1411c4b9c5c4ef0a155bf5defc74ae5
shubhbansal <shubhamindia18@gmail.com>
2020-12-17 10:39:20 +0300

    added vendors

--- customselectlist.py
+++ customselectlist.py
@@ -1,8 +1,9 @@
 list_vendor = [
-    "Organic German Bake Shop",
-    "DesiUtpadbyJaya",
-    "micheartisanbakery",
-    "Mr. Dairy",
+    "Organic German Bake Shop", "organicgermanbakeshop",
+    "DesiUtpadbyJaya", "Desi Utpad by Jaya",
+    "micheartisanbakery", "Miche Artisan Bake",
+    "Mr. Dairy","mrdairy",
+    "The Womens Company"
 ]
 
 list_created_via = [
