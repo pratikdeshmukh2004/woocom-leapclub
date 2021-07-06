09964184fd7d481d5d272cb79602fc47d1ebf62f
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2021-07-06 11:33:56 +0530

    sending pr without webhooks

--- slack_chennels.py
+++ slack_chennels.py
@@ -1,23 +1,23 @@
 # # This dict is for production....
-# CHANNELS = {
-#     "ORDER_NOTIFICATIONS": "orders-notifications",
-#     "PRODUCT_NOTIFICATIONS": "product-notifications",
-#     "VENDOR_WISE": "delivery-updates",
-#     "TAG_USER_ID": "<@U01HP4Q58S2>",
-#     "DAIRY_NOTIFICATIONS": "dairy-updates",
-#     "PAYMENT_NOTIFICATIONS": "payment-notifications",
-#     "SESSION_MESSAGES": 'session-messages'
-# }
+CHANNELS = {
+    "ORDER_NOTIFICATIONS": "orders-notifications",
+    "PRODUCT_NOTIFICATIONS": "product-notifications",
+    "VENDOR_WISE": "delivery-updates",
+    "TAG_USER_ID": "<@U01HP4Q58S2>",
+    "DAIRY_NOTIFICATIONS": "dairy-updates",
+    "PAYMENT_NOTIFICATIONS": "payment-notifications",
+    "SESSION_MESSAGES": 'session-messages'
+}
 #
 # # This dict is for testing....
-CHANNELS = {
-    "ORDER_NOTIFICATIONS": "flask-bot",
-    "PRODUCT_NOTIFICATIONS": "flask-bot",
-    "VENDOR_WISE": "flask-bot",
-    "TAG_USER_ID": "",
-    "DAIRY_NOTIFICATIONS": "flask-bot",
-    "PAYMENT_NOTIFICATIONS": "flask-bot",
-    "SESSION_MESSAGES": 'session-messages',
-    "PENDING_PAYMENT": 'flask-bot',
-    "PRODUCT_QUESTION_FEEDBACK": 'flask-bot'
-}
+# CHANNELS = {
+#     "ORDER_NOTIFICATIONS": "flask-bot",
+#     "PRODUCT_NOTIFICATIONS": "flask-bot",
+#     "VENDOR_WISE": "flask-bot",
+#     "TAG_USER_ID": "",
+#     "DAIRY_NOTIFICATIONS": "flask-bot",
+#     "PAYMENT_NOTIFICATIONS": "flask-bot",
+#     "SESSION_MESSAGES": 'session-messages',
+#     "PENDING_PAYMENT": 'flask-bot',
+#     "PRODUCT_QUESTION_FEEDBACK": 'flask-bot'
+# }
