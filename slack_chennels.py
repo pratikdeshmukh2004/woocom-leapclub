38226e36de2cd2e859380d38f5218c37d05b7fe6
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2021-07-06 11:39:07 +0530

    solved bug

--- slack_chennels.py
+++ slack_chennels.py
@@ -6,7 +6,9 @@ CHANNELS = {
     "TAG_USER_ID": "<@U01HP4Q58S2>",
     "DAIRY_NOTIFICATIONS": "dairy-updates",
     "PAYMENT_NOTIFICATIONS": "payment-notifications",
-    "SESSION_MESSAGES": 'session-messages'
+    "SESSION_MESSAGES": 'session-messages',
+    "PENDING_PAYMENT": 'pending-payments',
+
 }
 #
 # # This dict is for testing....
